66866aae7486863e66e916bdf306110cf80da365
faiz-82 <mrfaiz956@gmail.com>
2026-01-16 20:40:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # EC2 in us-east-1 (default provider)
 resource "aws_instance" "example3" {
-  ami           = var.ami_ids["us-west-1"]
+  ami           = var.ami_ids["us-east-1"]
   instance_type = var.instance_type
 
   tags = merge(var.tags, { Name = "Terraform Cloud" })
