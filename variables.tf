f807549562a52e624f3d1231523cca1b67199aa9
faiz-82 <mrfaiz956@gmail.com>
2026-01-16 20:19:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,16 +3,6 @@ variable "default_region" {
   type        = string
 }
 
-variable "us_east_1_region" {
-  description = "AWS us-east-1 region"
-  type        = string
-}
-
-variable "us_east_2_region" {
-  description = "AWS us-east-2 region"
-  type        = string
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
@@ -22,7 +12,6 @@ variable "ami_ids" {
   description = "AMI IDs per AWS region"
   type        = map(string)
 }
-
 
 variable "tags" {
   description = "Common tags for all resources"
