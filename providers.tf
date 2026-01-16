b70804bfbae1ef76fa78b8098a80907494612fa6
faiz-82 <mrfaiz956@gmail.com>
2026-01-16 20:41:51 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region = var.default_region
 }
 
-provider "aws" {
-  alias  = "us_east_1"
-  region = var.us_east_1_region
-}
-
-provider "aws" {
-  alias  = "us_east_2"
-  region = var.us_east_2_region
-}
