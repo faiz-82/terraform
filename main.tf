8c368eed26e2f100f1a17e2ed2f125d048ea2c04
Faiz <mrfaiz956@gmail.com>
2026-01-16 09:45:46 -0600

    uncomment

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-# # EC2 in us-east-1 (default provider)
-# resource "aws_instance" "example3" {
-#   ami           = var.ami_ids["us-east-1"]
-#   instance_type = var.instance_type
+# EC2 in us-east-1 (default provider)
+resource "aws_instance" "example3" {
+  ami           = var.ami_ids["us-east-1"]
+  instance_type = var.instance_type
 
-#   tags = merge(var.tags, { Name = "Github-Push-Instance" })
-# }
+  tags = merge(var.tags, { Name = "Github-Push-Instance" })
+}
