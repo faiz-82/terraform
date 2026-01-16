b14ba0cc4d3d8d0477c5a8f89042e5a670594887
faiz-82 <mrfaiz956@gmail.com>
2026-01-16 19:53:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # EC2 in us-east-1
 resource "aws_instance" "example1" {
-  provider      = aws.us_east_1
+  provider      = var.us_east_1
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  tags = merge(var.tags, { Name = "ExampleInstance1" })
+  tags = { Name = var.tag }
